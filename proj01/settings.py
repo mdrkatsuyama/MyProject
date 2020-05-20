4fc4cb7175e468001832ee3753700a83807f8f0c
Dr Katsuyama <zrcasista@gmail.com>
2020-05-20 20:09:27 +0900

    make it better

--- proj01/settings.py
+++ proj01/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '8hpd_u*%ne7#wsr113xz%y(2#5yii59dzwaxhzcbx(6t28)+75'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
